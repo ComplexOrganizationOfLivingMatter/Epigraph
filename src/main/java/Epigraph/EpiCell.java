57d0acd29df943502adcbaa29b0e3b1d42d3c788
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-13 15:32:04 +0100

    init epicell

---
+++ src/main/java/Epigraph/EpiCell.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package main.java.Epigraph;
+
+import java.util.ArrayList;
+
+/**
+ * @author Equipo
+ *
+ */
+public class EpiCell {
+	int id;
+	ArrayList<EpiCell> neighbours;
+	boolean valid_cell;
+	
+}
