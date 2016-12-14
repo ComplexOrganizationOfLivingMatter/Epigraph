b20ba5dc6fc6c938bf813e11d7c78d24991dea05
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-14 13:50:34 +0100

    processing image!!!! somewhat working

--- src/main/java/Epigraph/GraphletImage.java
+++ src/main/java/Epigraph/GraphletImage.java
@@ -24,10 +24,44 @@ public class GraphletImage {
 	 */
 	public GraphletImage(ImagePlus img) {
 		super();
-		this.raw_img = img;
 		EDM edm = new EDM();
-		//label image
-		img.show();
+		
+		if (!img.getChannelProcessor().isBinary()){
+			System.out.println("No binary image, improving...");
+			img.getChannelProcessor().autoThreshold();
+			img.show();
+		}
+		
+		int[][] pixels = img.getChannelProcessor().getIntArray();
+		int whitePixels = 0;
+		int blackPixels = 0;
+		for (int i = 0; i < img.getWidth(); i++) {
+			for (int j = 0; j < img.getHeight(); j++){
+				if (pixels[i][j] == 0)
+					blackPixels++;
+				else
+					whitePixels++;
+			}
+		}
+		if (blackPixels > whitePixels){
+			img.getChannelProcessor().invert();
+			img.show();
+		}
+		
+		this.raw_img = img;
+		
+		edm.run(img.getChannelProcessor());
 		edm.toWatershed(img.getChannelProcessor());
+		img.show();
+		this.l_img = img;
+		
+		pixels = img.getChannelProcessor().getIntArray();
+		
+		for (int i = 0; i < img.getWidth(); i++) {
+			for (int j = 0; j < img.getHeight(); j++){
+				System.out.println(pixels[i][j]);
+			}
+		}
+		img.show();
 	}
 }
