189d38d785e0638ba37cee85764bc8ba83f147cf
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-19 11:17:06 +0100

    constructor and variables

--- src/main/java/Epigraph/Orca.java
+++ src/main/java/Epigraph/Orca.java
@@ -4,9 +4,20 @@
 package Epigraph;
 
 /**
- * @author Equipo
+ * @author Pablo Vicente-Munuera
+ * 
+ * Adaptation of Orca (Orbit Counting Algorithm):
+ * A combinatorial approach to graphlet counting - by Tomaz Hocevar.
  *
  */
 public class Orca {
+	
+	int[][] adjacencyMatrix;
+	int[][] orbit; // orbit[x][o] - how many times does node x participate in orbit o
 
+	public Orca(int[][] adjacencyMatrix) {
+		super();
+		this.adjacencyMatrix = adjacencyMatrix;
+		this.orbit = new int[adjacencyMatrix.length][73];
+	}
 }
