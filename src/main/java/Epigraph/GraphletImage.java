ae2b750e794b4635cde1aee7568a6fd5034c9ee2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-21 12:33:47 +0100

    distance working but not correct

--- src/main/java/Epigraph/GraphletImage.java
+++ src/main/java/Epigraph/GraphletImage.java
@@ -91,7 +91,7 @@ public class GraphletImage {
 		MaximumFinder mxf = new MaximumFinder();
 		ByteProcessor btp = mxf.findMaxima(img.getChannelProcessor(), 0.5, MaximumFinder.SINGLE_POINTS, true);
 		img.setProcessor(btp);
-		img.show();
+		//img.show();
 		this.l_img = new ImagePlus("", img.getChannelProcessor().convertToFloat());
 		pixels = img.getChannelProcessor().getIntArray();
 		
@@ -130,6 +130,7 @@ public class GraphletImage {
 				percentageOfHexagonsOriginal++;
 			}
 		}
+		
 		System.out.println(percentageOfHexagons);
 		System.out.println(percentageOfHexagonsOriginal);
 		this.orcaProgram = null;
@@ -147,6 +148,24 @@ public class GraphletImage {
 		
 		System.out.println(numValidCells);
 		
+		// These are the graphlets we won't use on these configurations
+		int[] totalParcialGraphlets = {8, 14, 22, 23, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
+		int[] totalGraphlets = {49, 50, 62, 63, 64, 54, 55, 68, 69};
+		int[] basicGraphlets = {16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
+		int[] basicParcialGraphlets = {8, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
+		
+		ArrayList<Integer[]> graphletsFinal = new ArrayList<Integer[]>();
+		Integer[] actualGraphlets;
+		for ( EpiCell cell : this.cells) {
+			if (cell.isValid_cell_5()){
+				actualGraphlets = cell.getGraphletsInteger(totalGraphlets);
+				System.out.println(actualGraphlets);
+				graphletsFinal.add(actualGraphlets);
+			}
+		}
+		
+		float distanceGDDH = calculateGDDH(graphletsFinal);
+		System.out.println(distanceGDDH);
 	}
 	
 	/**
@@ -266,11 +285,13 @@ public class GraphletImage {
 	 * 
 	 * @return
 	 */
-	public float distanceGDDH(){
-		int[][] hexagonReference = {{6, 18, 9, 6, 54, 54, 6, 2, 0, 12, 24, 12, 6, 6, 0, 162, 162, 81, 18, 36, 18, 18, 0, 0, 48, 24, 48, 36, 36, 72, 36, 0, 0, 0, 0, 0, 0, 0, 0, 6, 12, 6, 6, 12, 3, 12, 12, 12, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 12, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+	public float calculateGDDH(ArrayList<Integer[]> graphletsFinal){
+		ArrayList<Integer[]> hexagonReference = new ArrayList<Integer[]>();
+		Integer[] hexagonRefInt = {6, 18, 9, 6, 54, 54, 6, 2, 0, 12, 24, 12, 6, 6, 0, 162, 162, 81, 18, 36, 18, 18, 0, 0, 48, 24, 48, 36, 36, 72, 36, 0, 0, 0, 0, 0, 0, 0, 0, 6, 12, 6, 6, 12, 3, 12, 12, 12, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 12, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		
+		hexagonReference.add(hexagonRefInt);
 		ArrayList<HashMap<Integer, Float>> graphletFreqRef = scaleGraphletDists(hexagonReference);
-		ArrayList<HashMap<Integer, Float>> graphletFreqImage = scaleGraphletDists(this.orcaProgram.getOrbit());
+		ArrayList<HashMap<Integer, Float>> graphletFreqImage = scaleGraphletDists(graphletsFinal);
 		
 		float[] orbitDist = new float[this.cells.get(0).getGraphlets().length];
 		
@@ -280,7 +301,8 @@ public class GraphletImage {
 			
 			//Compute the distance among the orbits
 			float sumDistances = (float) 0.0;
-			Set<Integer> allDegrees = values1.keySet();
+			HashSet<Integer> allDegrees = new HashSet<Integer>();
+			allDegrees.addAll(values1.keySet());
 			allDegrees.addAll(values2.keySet());
 			
 			for (Integer degree : allDegrees){
@@ -339,7 +361,7 @@ public class GraphletImage {
 	 * @param signatures
 	 * @return
 	 */
-	private ArrayList<HashMap<Integer,Float>> scaleGraphletDists(int[][] signatures){
+	private ArrayList<HashMap<Integer,Float>> scaleGraphletDists(ArrayList<Integer[]> signatures){
 		ArrayList<HashMap<Integer, Float>> distributions = new ArrayList<HashMap<Integer, Float>>();
 		
 		HashMap<Integer, Float> graphletsValues;
@@ -347,10 +369,10 @@ public class GraphletImage {
 		for (int numGraphlet = 0; numGraphlet < TOTALGRAPHLETS; numGraphlet++){
 			graphletsValues = new HashMap<Integer, Float>();
 			
-			for (int numNode = 0; numNode < signatures.length; numNode++){
-				actualValue = graphletsValues.putIfAbsent(signatures[numNode][numGraphlet], (float) 1);
+			for (int numNode = 0; numNode < signatures.size(); numNode++){
+				actualValue = graphletsValues.putIfAbsent(signatures.get(numNode)[numGraphlet], (float) 1);
 				if (actualValue != null){
-					graphletsValues.put(signatures[numNode][numGraphlet], actualValue + 1);
+					graphletsValues.put(signatures.get(numNode)[numGraphlet], actualValue + 1);
 				}
 			}
 			graphletsValues.remove(0);
