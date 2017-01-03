e990cad01d4de0c2f7b1f9240c48126ce8c0eaf8
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-01-03 16:26:54 +0100

    refactoring

--- src/main/java/Epigraph/GraphletImage.java
+++ src/main/java/Epigraph/GraphletImage.java
@@ -30,11 +30,11 @@ import util.FindConnectedRegions;
  *
  */
 public class GraphletImage {
-	ImagePlus raw_img;
-	ImagePlus l_img;
-	ArrayList<EpiCell> cells;
-	int[][] adjacencyMatrix;
-	Orca orcaProgram;
+	private ImagePlus raw_img;
+	private ImagePlus l_img;
+	private ArrayList<EpiCell> cells;
+	private int[][] adjacencyMatrix;
+	private Orca orcaProgram;
 	
 	public static int CIRCLE_SHAPE = 0;
 	public static int SQUARE_SHAPE = 1;
@@ -44,6 +44,12 @@ public class GraphletImage {
 	//Hexagonal reference
 	public static Integer[] hexagonRefInt = {6, 18, 9, 6, 54, 54, 6, 2, 0, 12, 24, 12, 6, 6, 0, 162, 162, 81, 18, 36, 18, 18, 0, 0, 48, 24, 48, 36, 36, 72, 36, 0, 0, 0, 0, 0, 0, 0, 0, 6, 12, 6, 6, 12, 3, 12, 12, 12, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 12, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	
+	// These are the graphlets we won't use on these configurations
+	private static int[] totalParcialGraphlets = {8, 14, 22, 23, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
+	private static int[] totalGraphlets = {49, 50, 62, 63, 64, 54, 55, 68, 69};
+	private static int[] basicGraphlets = {16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
+	private static int[] basicParcialGraphlets = {8, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
+		
 	/**
 	 * @param img image
 	 */
@@ -153,11 +159,6 @@ public class GraphletImage {
 		
 		System.out.println(numValidCells);
 		
-		// These are the graphlets we won't use on these configurations
-		int[] totalParcialGraphlets = {8, 14, 22, 23, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
-		int[] totalGraphlets = {49, 50, 62, 63, 64, 54, 55, 68, 69};
-		int[] basicGraphlets = {16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
-		int[] basicParcialGraphlets = {8, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72};
 		
 		int[] graphletsWeDontWant;
 		switch (modeNumGraphlets) {
@@ -305,12 +306,11 @@ public class GraphletImage {
 	 * 
 	 * @return
 	 */
-	public float calculateGDDH(ArrayList<Integer[]> graphletsFinal){
-		ArrayList<Integer[]> hexagonReference = new ArrayList<Integer[]>();
+	public float calculateGDD(ArrayList<Integer[]> graphletsFinal, vectorReferenceInt){
+		ArrayList<Integer[]> distanceReference = new ArrayList<Integer[]>();
 		
-		
-		hexagonReference.add(hexagonRefInt);
-		ArrayList<HashMap<Integer, Float>> graphletFreqRef = scaleGraphletDists(hexagonReference);
+		distanceReference.add(vectorReferenceInt);
+		ArrayList<HashMap<Integer, Float>> graphletFreqRef = scaleGraphletDists(distanceReference);
 		ArrayList<HashMap<Integer, Float>> graphletFreqImage = scaleGraphletDists(graphletsFinal);
 		
 		float[] orbitDist = new float[this.cells.get(0).getGraphlets().length];
