8cefe2bce549bfceb05fcdc0ca2406d8b368c67b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-01-09 12:12:46 +0100

    working version

--- src/main/java/epigraph/epigraph.java
+++ src/main/java/epigraph/epigraph.java
@@ -9,6 +9,8 @@ import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.swing.SwingUtilities;
+
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -25,6 +27,7 @@ public class Epigraph implements PlugIn {
 	
 	/** image to be used in the training */
 	private ImagePlus raw_img = null;
+	JPanelModel mainWindow;
 	
 //	private class CustomWindow extends StackWindow
 //	{
@@ -62,53 +65,41 @@ public class Epigraph implements PlugIn {
 	 */
 	public void run(String arg)
 	{
-		
-//
-		//get current image
-		if (null == WindowManager.getCurrentImage())
-		{
-			raw_img = IJ.openImage();
-			if (null == raw_img) return; // user canceled open dialog
-		}
-		else
-		{
-			raw_img = WindowManager.getCurrentImage().duplicate();
-			raw_img.setSlice( 
-					WindowManager.getCurrentImage().getSlice() );
-		}
-		
-		//System.out.println(gddh)
-		ExcelClass ec=new ExcelClass();
-		ec.importData("D:/Pedro/Graphlet/pruebas exportar u3d/TotalParcial_3Ddimensions_test.xls");
-				
-		
-		for (int i = 0; i < ec.getB().size(); i++) {
-			System.out.println(ec.getGddh().get(i));
-		}
+		//Build GUI
+		SwingUtilities.invokeLater(
+				new Runnable() {
+					public void run() {
+						mainWindow = new JPanelModel();
+						mainWindow.createAndShowGUI();
+					}
+				});
 
-		ec.exportData("D:/Pedro/Graphlet/pruebas exportar u3d/Export_test.xls");
+//		//get current image
+//		if (null == WindowManager.getCurrentImage())
+//		{
+//			raw_img = IJ.openImage();
+//			if (null == raw_img) return; // user canceled open dialog
+//		}
+//		else
+//		{
+//			raw_img = WindowManager.getCurrentImage().duplicate();
+//			raw_img.setSlice( 
+//					WindowManager.getCurrentImage().getSlice() );
+//		}
+//		
+//		//System.out.println(gddh)
+//		ExcelClass ec=new ExcelClass();
+//		ec.importData("D:/Pedro/Graphlet/pruebas exportar u3d/TotalParcial_3Ddimensions_test.xls");
+//				
+//		
+//		for (int i = 0; i < ec.getB().size(); i++) {
+//			System.out.println(ec.getGddh().get(i));
+//		}
+//
+//		ec.exportData("D:/Pedro/Graphlet/pruebas exportar u3d/Export_test.xls");
 		
 		//GraphletImage graphletImage = new GraphletImage(raw_img);
-		
-		
-		
-//		wekaSegmentation.setTrainingImage(trainingImage);
-		
-//		// The display image is a copy of the training image (single image or stack)
-//		displayImage = trainingImage.duplicate();
-//		displayImage.setSlice( trainingImage.getSlice() );
-//		displayImage.setTitle( Weka_Segmentation.PLUGIN_NAME + " " + Weka_Segmentation.PLUGIN_VERSION );
 
-//		ij.gui.Toolbar.getInstance().setTool(ij.gui.Toolbar.FREELINE);
-//
-//		//Build GUI
-//		SwingUtilities.invokeLater(
-//				new Runnable() {
-//					public void run() {
-////						win = new CustomWindow(displayImage);
-////						win.pack();
-//					}
-//				});
 	}
 	
 	
