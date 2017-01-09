a839c2cbd6cf083d3c3923a9eda0efceae731be1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-01-09 15:40:15 +0100

    thumbnails working, now creating a roi and buttons

--- src/main/java/epigraph/ImageWindow.java
+++ src/main/java/epigraph/ImageWindow.java
@@ -23,6 +23,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 /**
  * 
@@ -53,46 +55,16 @@ public class ImageWindow extends JFrame {
 				returnGraphletImages();
 			}
 		});
-		setBounds(100, 100, 562, 531);
+		setBounds(100, 100, 935, 798);
 		contentPane = new JPanel();
 		contentPane.setBorder(null);
 		setContentPane(contentPane);
-		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
-				ColumnSpec.decode("21px"),
-				ColumnSpec.decode("147px"),
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("184px"),
-				RowSpec.decode("33px"),
-				RowSpec.decode("25px"),
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,}));
 		
 		
 		ImagePlus imgToShow = new ImagePlus("", raw_img.getChannelProcessor());
 		BufferedImage thumbnail;
 		try {
-			thumbnail = Thumbnails.of(raw_img.getBufferedImage()).height(400).asBufferedImage();
+			thumbnail = Thumbnails.of(raw_img.getBufferedImage()).height(512).asBufferedImage();
 			imgToShow.setImage(thumbnail);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -102,20 +74,52 @@ public class ImageWindow extends JFrame {
 		canvas.setShowCursorStatus(false);
 		canvas.setShowAllROIs(false);
 		canvas.setPaintPending(false);
-		canvas.setScaleToFit(true);
 		canvas.setCustomRoi(false);
-		Roi roi;
-		roi = new Roi(0, 0, imgToShow);
-		raw_img.setRoi(roi);
-		contentPane.add(canvas, "4, 2, 7, 13, center, top");
+		canvas.setSize(imgToShow.getWidth(), imgToShow.getHeight());
 		
-		JButton btnNewButton = new JButton("Calculate graphlets!");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnCalculateGraphlets = new JButton("Calculate graphlets!");
+		btnCalculateGraphlets.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				newGraphletImages.add(new GraphletImage(raw_img));
 			}
 		});
-		contentPane.add(btnNewButton, "6, 18, center, default");
+		
+		JButton btnCreateRoi = new JButton("Create RoI");
+		btnCreateRoi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Roi roi;
+				roi = new Roi(4, 2, imgToShow);
+				raw_img.setRoi(roi);
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(244)
+					.addComponent(canvas, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(581))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(790, Short.MAX_VALUE)
+					.addComponent(btnCreateRoi)
+					.addGap(42))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addGap(432)
+					.addComponent(btnCalculateGraphlets, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(348))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(7)
+					.addComponent(canvas, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(121)
+					.addComponent(btnCreateRoi)
+					.addGap(490)
+					.addComponent(btnCalculateGraphlets)
+					.addGap(32))
+		);
+		contentPane.setLayout(gl_contentPane);
 		
 	}
 
