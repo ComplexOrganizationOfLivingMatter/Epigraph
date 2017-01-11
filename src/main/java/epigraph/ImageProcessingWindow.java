0614da53a6831549f1c6fa0c87cade64130ab292
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-01-11 14:53:31 +0100

    color picker on imageProcessing

--- src/main/java/epigraph/ImageProcessingWindow.java
+++ src/main/java/epigraph/ImageProcessingWindow.java
@@ -9,12 +9,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import fiji.util.gui.OverlayedImageCanvas;
 import ij.ImagePlus;
 import net.coobird.thumbnailator.Thumbnails;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 /**
  * 
@@ -33,6 +37,7 @@ public class ImageProcessingWindow extends JFrame {
 	private JPanel contentPane;
 	
 	private ArrayList<GraphletImage> newGraphletImages;
+	private JTextField tfImageName;
 
 	/**
 	 * Create the frame.
@@ -83,6 +88,30 @@ public class ImageProcessingWindow extends JFrame {
 		contentPane.add(btnCreateRoi);
 		contentPane.add(btnCalculateGraphlets);
 		
+		tfImageName = new JTextField();
+		tfImageName.setBounds(752, 257, 146, 26);
+		contentPane.add(tfImageName);
+		tfImageName.setColumns(10);
+		
+		JLabel lblImageName = new JLabel("Image Name");
+		lblImageName.setBounds(751, 235, 113, 20);
+		contentPane.add(lblImageName);
+		
+		JPanel colorPicked = new JPanel();
+		colorPicked.setBackground(Color.BLACK);
+		colorPicked.setBounds(752, 354, 113, 25);
+		contentPane.add(colorPicked);
+		
+		JButton btnPickAColor = new JButton("Pick a color");
+		btnPickAColor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Color c = JColorChooser.showDialog(null, "Choose a Color", colorPicked.getBackground());
+			      if (c != null)
+			    	  colorPicked.setBackground(c);
+			}
+		});
+		btnPickAColor.setBounds(749, 309, 115, 29);
+		contentPane.add(btnPickAColor);
 	}
 
 	private void createROI(ImagePlus imgToShow) {
