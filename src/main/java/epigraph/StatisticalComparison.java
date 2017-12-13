c23345bf6d36fec040ff73438c6dbfeb1381b413
Pablo Vicente Munuera <pvicente1@us.es>
2017-12-13 12:57:50 +0100

    Euclidean distance implemented between the two means

--- src/main/java/epigraph/StatisticalComparison.java
+++ src/main/java/epigraph/StatisticalComparison.java
@@ -4,8 +4,14 @@
 package epigraph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import epigraph.LibMahalanobis.Utils;
+
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
+import org.apache.commons.math3.stat.correlation.Covariance;
+import org.apache.commons.math3.stat.descriptive.MultivariateSummaryStatistics;
 
 
 /**
@@ -35,7 +41,7 @@ final class StatisticalComparison {
 	 * @param newGroup
 	 * @return
 	 */
-	public static double compareGroupsOfImages(ArrayList<BasicGraphletImage> originalGroup, ArrayList<BasicGraphletImage> newGroup){
+	public static double[] compareGroupsOfImages(ArrayList<BasicGraphletImage> originalGroup, ArrayList<BasicGraphletImage> newGroup){
 	
 		double[][] originalData = create4DMatrix(originalGroup);
 		ArrayList<BasicGraphletImage> originalPlusNewGroup = new ArrayList<BasicGraphletImage>();
@@ -47,13 +53,41 @@ final class StatisticalComparison {
 		double[] stdDevs = Utils.getStdDev(originalData);
 		double[] stdDevsNewData = Utils.getStdDev(originalPlusNewData);
 		
+		double[] originalMean = Utils.getMean(originalData);
+		double[] newGroupMean = Utils.getMean(create4DMatrix(newGroup));
+		
+		EuclideanDistance euDis = new EuclideanDistance();
+		double distanceBetweenMeans = euDis.compute(originalMean, newGroupMean);
+		
 		double[] stdDiff = new double[stdDevs.length];
 		
 		for (int numDimension = 0; numDimension < stdDevs.length; numDimension++){
 			stdDiff[numDimension] = stdDevsNewData[numDimension] / stdDevs[numDimension];
 		}
 		
-		return Utils.getMean(stdDiff);
+		//RealMatrix rmOriginal = getCovariance(originalData);
+		//RealMatrix rmOriginalPlusNew = getCovariance(originalPlusNewData);
+		
+		double[] stdDiffAndDistance = {Utils.getMean(stdDiff), distanceBetweenMeans};
+		return stdDiffAndDistance;
+	}
+
+	/**
+	 * @param matrix4D
+	 * @return 
+	 */
+	public static RealMatrix getCovariance(double[][] matrix4D) {
+		MultivariateSummaryStatistics statsOriginal = new MultivariateSummaryStatistics(MAX_DIMENSIONS, false);
+		double[] newRow = new double[MAX_DIMENSIONS]; 
+		for (int numImg = 0; numImg < matrix4D[0].length; numImg++){
+			newRow[0] = matrix4D[0][numImg];
+			newRow[1] = matrix4D[1][numImg];
+			newRow[2] = matrix4D[2][numImg];
+			newRow[3] = matrix4D[3][numImg];
+			statsOriginal.addValue(newRow);
+		}
+		
+		return statsOriginal.getCovariance();
 	}
 
 	/**
@@ -68,7 +102,7 @@ final class StatisticalComparison {
 			originalData[0][numRow] = groupOfImages.get(numRow).getDistanceGDDH();
 			originalData[1][numRow] = groupOfImages.get(numRow).getDistanceGDDRV();
 			originalData[2][numRow] = groupOfImages.get(numRow).getDistanceGDDV5();
-			originalData[3][numRow] = groupOfImages.get(numRow).getPercentageOfHexagons();
+			originalData[3][numRow] = groupOfImages.get(numRow).getPercentageOfHexagons() / 100; //Normalization
 		}
 		
 		return originalData;
