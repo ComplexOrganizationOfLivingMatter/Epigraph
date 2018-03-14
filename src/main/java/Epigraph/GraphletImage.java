e28c4fa0c361c1ba33d6585c38b3dc5fab2c7e27
Pablo Vicente Munuera <pvicente1@us.es>
2018-03-14 14:51:39 +0100

    an incorrect orbit

--- src/main/java/Epigraph/GraphletImage.java
+++ src/main/java/Epigraph/GraphletImage.java
@@ -74,7 +74,7 @@ public class GraphletImage extends BasicGraphletImage implements Cloneable {
 	private static int[] totalParcialGraphlets = { 8, 14, 22, 23, 36, 37, 38, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
 			62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72 };
 	private static int[] totalGraphlets = { 49, 50, 62, 63, 64, 54, 55, 68, 69 };
-	private static int[] basicGraphlets = { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+	private static int[] basicGraphlets = { 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
 			35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61,
 			62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72 };
 	private static int[] basicParcialGraphlets = { 8, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
