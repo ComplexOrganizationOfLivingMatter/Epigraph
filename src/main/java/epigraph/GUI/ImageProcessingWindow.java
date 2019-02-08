d33b8f2d9028fd72e55c2b22a13fd9c3e3d67b98
anttagjan <38495085+anttagjan@users.noreply.github.com>
2019-02-08 17:07:42 +0100

    Testing scrollbar

--- src/main/java/epigraph/GUI/ImageProcessingWindow.java
+++ src/main/java/epigraph/GUI/ImageProcessingWindow.java
@@ -11,9 +11,12 @@ import java.awt.Insets;
 import java.awt.Panel;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Scrollbar;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.io.BufferedWriter;
@@ -73,7 +76,7 @@ import util.opencsv.CSVWriter;
  * 
  * @author Pedro Gomez-Galvez, Pablo Vicente-Munuera
  */
-public class ImageProcessingWindow extends ImageWindow implements ActionListener {
+public class ImageProcessingWindow extends ImageWindow implements ActionListener, AdjustmentListener {
 
 	private static final long serialVersionUID = 1L;
 	private static int MIN_GRAPHLETS_ON_IMAGE = 15;
@@ -136,6 +139,7 @@ public class ImageProcessingWindow extends ImageWindow implements ActionListener
 
 	private DiagramsData diagramsData;
 	private String initialDirectory;
+	private Scrollbar sliceSelector; 
 
 	/**
 	 * Constructor
@@ -156,6 +160,18 @@ public class ImageProcessingWindow extends ImageWindow implements ActionListener
 
 		canvas = (CustomCanvas) super.getCanvas();
 		
+    // The scrollbar 
+    int stackSize = raw_img.getStackSize(); 
+    sliceSelector = new Scrollbar(); 
+    sliceSelector.addAdjustmentListener(this); 
+    sliceSelector.setFocusable(false); 
+    sliceSelector.setMinimum(1); 
+    sliceSelector.setMaximum(stackSize + 1); 
+    sliceSelector.setOrientation(Scrollbar.HORIZONTAL); 
+    sliceSelector.setVisible(true); 
+    add(sliceSelector, canvas); 
+    setVisible(true); 
+
 		newGraphletImages = new ArrayList<GraphletImage>();
 
 		tableInf = tableInfo;
@@ -172,6 +188,11 @@ public class ImageProcessingWindow extends ImageWindow implements ActionListener
 		threadFinished = false;
 	}
 
+  public synchronized void adjustmentValueChanged(AdjustmentEvent 
+e) { 
+            int z = sliceSelector.getValue(); 
+            imp.setSlice(z); 
+  } 
 	/**
 	 * initialize GUI and configure panels
 	 */
